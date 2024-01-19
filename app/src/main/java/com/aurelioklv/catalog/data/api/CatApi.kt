fbbbea51116f64faeea01eb8c98e61a38a9e1482
Aurelio Killian Lexi Verrill <aurelioklv@gmail.com>
2024-01-19 20:57:03 +0700

    feat: add breed model

--- app/src/main/java/com/aurelioklv/catalog/data/api/CatApi.kt
+++ app/src/main/java/com/aurelioklv/catalog/data/api/CatApi.kt
@@ -1,5 +1,6 @@
 package com.aurelioklv.catalog.data.api
 
+import com.aurelioklv.catalog.data.model.Breed
 import com.aurelioklv.catalog.data.model.Cat
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -10,4 +11,9 @@ interface CatApi {
         @Query("limit") limit: Int = 10,
         @Query("has_breeds") hasBreed: Int = 0
     ): List<Cat>
+
+    @GET("breeds")
+    suspend fun getBreeds(
+        @Query("limit") limit: Int? = null,
+    ): List<Breed>
 }
--- app/src/main/java/com/aurelioklv/catalog/data/model/Cat.kt
+++ app/src/main/java/com/aurelioklv/catalog/data/model/Cat.kt
@@ -5,6 +5,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class Cat(
+    val breeds: List<Breed>? = null,
     val id: String,
 
     @SerialName("url")

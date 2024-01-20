bee9fff87ad3d5ed417c64c458ce7570ed6c63b6
Aurelio Killian Lexi Verrill <aurelioklv@gmail.com>
2024-01-20 11:16:17 +0700

    test: add api test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -85,6 +85,7 @@ dependencies {
     implementation("io.coil-kt:coil-compose:2.5.0")
 
     testImplementation("junit:junit:4.13.2")
+    testImplementation("com.squareup.okhttp3:mockwebserver:4.12.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
---
+++ app/src/test/java/com/aurelioklv/catalog/CatApiTest.kt
@@ -0,0 +1,75 @@
+package com.aurelioklv.catalog
+
+import com.aurelioklv.catalog.data.api.CatApi
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Before
+import org.junit.Test
+import retrofit2.Retrofit
+import retrofit2.create
+import java.io.File
+
+class CatApiTest {
+    private lateinit var mockWebServer: MockWebServer
+    private lateinit var catApi: CatApi
+
+    @Before
+    fun setUp() {
+        mockWebServer = MockWebServer()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl(mockWebServer.url("/"))
+            .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+            .build()
+
+        catApi = retrofit.create()
+    }
+
+    @After
+    fun tearDown() {
+        mockWebServer.shutdown()
+    }
+
+    @Test
+    fun getBreeds() = runBlocking {
+        val responseJson = readJsonFromFile("src/test/resources/breeds.json")
+        val response = MockResponse().setBody(responseJson)
+
+        mockWebServer.enqueue(response)
+
+        val breeds = catApi.getBreeds()
+
+        assertNotNull(responseJson)
+        assertNotNull(breeds)
+        assertEquals("abys", breeds.first().id)
+    }
+
+    @Test
+    fun getCatWithBreed() = runBlocking {
+        val responseJson = readJsonFromFile("src/test/resources/cat_with_breeds.json")
+        val response = MockResponse().setBody(responseJson)
+
+        mockWebServer.enqueue(response)
+
+        val catWithBreeds = catApi.getCats()
+        val expectedBreeds = listOf("chee", "ragd", "siam", "snow", "sphy")
+
+        assertEquals(5, catWithBreeds.size)
+        catWithBreeds.forEachIndexed { index, cat ->
+            assertNotNull(cat.breeds)
+            assertEquals(expectedBreeds[index], cat.breeds!!.first().id)
+        }
+    }
+
+    private fun readJsonFromFile(filePath: String): String {
+        val file = File(filePath)
+        return file.readText()
+    }
+}
